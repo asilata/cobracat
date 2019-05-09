032ab2c79ba8e6482b7e0c15b79cc80a466ba71c
Asilata Bapat (ANU) <asilata@gmail.com>
2019-05-09 10:08:53 +1000

    Added some comments to the q-deformed plotting code.

--- deformed-sl2.py
+++ deformed-sl2.py
@@ -1,10 +1,11 @@
 import matplotlib.pyplot as plt
-q = 1
-z0 = 1
-N = 100000
-pointsDict = {}
+q = 1 # Value of the deformation parameter
+z0 = 1 # Compute orbit of this point
+N = 100000 # Apply up to N combinations of s1 and t2
+pointsDict = {} # Initially empty dictionary that will hold the values of the computations.
 
 def s1(z):
+    """Computes the action of the q-matrix $\sigma_1$. Returns None if the output is the point at infinity."""
     if z is None:
         return 1
     elif q+z == 0:
@@ -13,12 +14,14 @@ def s1(z):
         return z/float(q+z)
 
 def t2(z):
+    """Computes the action of the q-matrix $\sigma_2^{-1}. Returns None if the outpute is the point at infinity."""
     if z is None:
         return None
     else:
         return 1 + z/q
 
 def constructPoints():
+    """Populate pointsDict with (a,v) where v is the a'th computed point in the orbit. The structure is a binary tree where the left child of a node is the value of s1 and the right child of a node is the value of t2."""
     c = 1
     while c < N:
         if c == 1:
@@ -32,6 +35,7 @@ def constructPoints():
             pointsDict[c] = t2(z)
         c = c+1
 
+# Run everything: populate the points, and then plot the values.        
 constructPoints()
 vs = pointsDict.values()
 ks = [0 for x in vs]
